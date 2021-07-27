d995c8e1be54b084437efe55f6ad31f1f13705bb
Diamond-D0gs <32776854+Diamond-D0gs@users.noreply.github.com>
2021-07-27 15:42:27 -0400

    Some corrections

--- src/main.c
+++ src/main.c
@@ -12,11 +12,12 @@ Vec3f Barycentric2D(Vec2i triangle[], const float area, Vec2i P);
 void RasterizeTriangle(bmp_img* img, Vertice verts[], int index[], int indexLoc);
 
 int main(void) {
-    Vertice verts[4] = {{{12, 12, 0}, {255, 0, 0}, {0, 0}},
-                        {{12, 1012, 0}, {0, 255, 0}, {1, 0}},
-                        {{1012, 1012, 0}, {0, 0, 255}, {0, 1}},
-                        {{1012, 12, 0}, {255, 255, 255}, {1, 1}}};
-    int index[6] = {0, 1, 2, 1, 3, 2};
+    Vertice verts[4] = {
+        {{12, 12, 0}, {255, 0, 0}, {0, 0}},
+        {{1012, 12, 0}, {0, 0, 255}, {1, 0}},
+        {{12, 1012, 0}, {0, 255, 0}, {0, 1}},
+        {{1012, 1012, 0}, {255, 255, 255}, {1, 1}}};
+    int index[6] = {0, 2, 3, 0, 3, 1};
 
     bmp_img img;
     bmp_img_init_df (&img, WIDTH, HEIGHT);
@@ -57,22 +58,25 @@ Vec3f Barycentric2D(Vec2i triangle[], const float area, Vec2i P) {
     Vec3f bary;
 
     // ABP
-    bary.x = EdgeFunction(triangle[0], triangle[1], P) / area;
+    bary.u = EdgeFunction(triangle[0], triangle[1], P) / area;
     // BCP
-    bary.y = EdgeFunction(triangle[1], triangle[2], P) / area;
+    bary.v = EdgeFunction(triangle[1], triangle[2], P) / area;
     // CAP
-    bary.z = EdgeFunction(triangle[2], triangle[0], P) / area;
+    bary.w = EdgeFunction(triangle[2], triangle[0], P) / area;
 
     return bary;
 }
 
 void RasterizeTriangle(bmp_img* img, Vertice verts[], int index[], int indexLoc) {
-    Vec2i triangle[3];
-    triangle[0] = (Vec2i){verts[indexLoc].pos.x, verts[indexLoc].pos.y};
-    triangle[1] = (Vec2i){verts[indexLoc+1].pos.x, verts[indexLoc+1].pos.y};
-    triangle[2] = (Vec2i){verts[indexLoc+2].pos.x, verts[indexLoc+2].pos.y};
+    Vec2i triangle[3] = {
+        (Vec2i){verts[index[indexLoc]].pos.x, verts[index[indexLoc]].pos.y},
+        (Vec2i){verts[index[indexLoc+1]].pos.x, verts[index[indexLoc+1]].pos.y},
+        (Vec2i){verts[index[indexLoc+2]].pos.x, verts[index[indexLoc+2]].pos.y}};
+
+    printf("{%i, %i}, {%i, %i}, {%i, %i}\n", triangle[0].x, triangle[0].y, triangle[1].x, triangle[1].y, triangle[2].x, triangle[2].y);
     
     float area = EdgeFunction(triangle[0], triangle[1], triangle[2]);
+    printf("%f\n", area);
     if (area > 0) {
         Vec2i bboxmin, bboxmax;
         BoundingBox2D(triangle, &bboxmin, &bboxmax);
@@ -84,16 +88,13 @@ void RasterizeTriangle(bmp_img* img, Vertice verts[], int index[], int indexLoc)
             for (int x = bboxmin.x; x <= bboxmax.x; ++x) {
                 Vec3f bary = Barycentric2D(triangle, area, (Vec2i){x, y});
                 if (bary.u >= 0 && bary.v >= 0 && bary.w >= 0) {
-                    RGB color;
-                        color.r = (bary.u * verts[indexLoc].color.r) + (bary.v * verts[indexLoc+1].color.r) + (bary.w * verts[indexLoc+2].color.r);
-                        color.g = (bary.u * verts[indexLoc].color.g) + (bary.v * verts[indexLoc+1].color.g) + (bary.w * verts[indexLoc+2].color.g);
-                        color.b = (bary.u * verts[indexLoc].color.b) + (bary.v * verts[indexLoc+1].color.b) + (bary.w * verts[indexLoc+2].color.b);
-                        bmp_pixel_init (&img->img_pixels[y][x], color.r, color.g, color.b);
+                    RGB color = {
+                        color.r = (bary.u * verts[index[indexLoc]].color.r) + (bary.v * verts[index[indexLoc+1]].color.r) + (bary.w * verts[index[indexLoc+2]].color.r),
+                        color.g = (bary.u * verts[index[indexLoc]].color.g) + (bary.v * verts[index[indexLoc+1]].color.g) + (bary.w * verts[index[indexLoc+2]].color.g),
+                        color.b = (bary.u * verts[index[indexLoc]].color.b) + (bary.v * verts[index[indexLoc+1]].color.b) + (bary.w * verts[index[indexLoc+2]].color.b)};
+                    bmp_pixel_init (&img->img_pixels[y][x], color.r, color.g, color.b);
                 }
             }
         }
-    }
-    else {
-        printf("piss\n");
     }
 }
